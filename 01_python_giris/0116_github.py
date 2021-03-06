d0b814ab169b3f11b5e55cd6e31f2e9cf374ae0b
azizbektas <azikocan97023@gmail.com>
2021-03-06 23:25:21 +0300

    github-v8

--- 01_python_giris/0116_github.py
+++ 01_python_giris/0116_github.py
@@ -20,7 +20,7 @@ Adım 5 → Authenticaion/Authorization işlemi için token oluştur.
 https://github.com/settings/tokens → Generate New Token
 
 Adım 6 → İlk gönderimde yada değişiklik anında her gönderim için Commit and Push yapılmalı
-Views and More Actions → Commit | Yada | Source Control → Commit buton
+Views and More Actions → Commit | Yada | Source Control → Commit buton → Commit'e Bir İsim Ver
 Views and More Actions → Push | Yada | Durum Çubuğundaki → Synchronize Changes buton
 """
 #endregion
