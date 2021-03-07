d5d7ad92abfc8e067a0f19b1025103e4c7a8b1e5
azizbektas <azikocan97023@gmail.com>
2021-03-07 14:05:05 +0300

    070321-1405

--- 01_python_giris/0115_operatorler.py
+++ 01_python_giris/0115_operatorler.py
@@ -85,12 +85,3 @@ print(15%4%2)   #% left-side binding
 print(2**2**3)  #** right-side binding
 """
 #endregion
-
-
-"""
-print(10*(8*3)/(8%3))
-print(3-5/7+2*3*7-2/(3+5))
-print((5/1**2)**0.5)
-print((1+3)**(1/2)-(2**2)/(1**2))
-print(3**2 + 1**2 + 3/1)
-"""
