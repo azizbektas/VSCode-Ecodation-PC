ad311b7188b08633d29a66e55d4f91433d35a537
azizbektas <azikocan97023@gmail.com>
2021-03-08 22:50:09 +0300

    030821-2250

--- 01_python_giris/0117_github.py
+++ 01_python_giris/0117_github.py
@@ -16,10 +16,13 @@ View → Comment Palette → arama: "Add Remote"
 Repository Url: "repo url'sini ekle" https://github.com/azizbektas/VSCode-Ecodation-PC.git
 Remote Name: "remote ismi ekle" VSCode-Ecodation-PC
 
-Adım 5 → Authenticaion/Authorization işlemi için token oluştur.
+Adım 5 → Authenticaion/Authorization işlemi için kalıcı token oluştur.
 https://github.com/settings/tokens → Generate New Token
 
-Adım 6 → İlk gönderimde yada değişiklik anında her gönderim için Commit and Push yapılmalı
+Adım 6 → Authenticaion/Authorization işlemi için Account yöntemi ile geçici token oluştur
+Turn on Settings Sync → Turn on → Sign in → Github → Authorized Github → VSCode Aç
+
+Adım 7 → İlk gönderimde yada değişiklik anında her gönderim için Commit and Push yapılmalı
 Views and More Actions → Commit | Yada | Source Control → Commit buton → Commit'e Bir İsim Ver "060321-2330"
 Views and More Actions → Push | Yada | Status Bar'daki → Synchronize Changes buton, Login İçin Token Gir 
 """
