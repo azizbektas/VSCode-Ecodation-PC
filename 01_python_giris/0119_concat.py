39100de9304ccebbea176c8cf23c7347013630f4
azizbektas <azikocan97023@gmail.com>
2021-03-08 21:08:55 +0300

    030821-2108

---
+++ 01_python_giris/0119_concat.py
@@ -0,0 +1,20 @@
+#region
+#concat - değişken birleştirme
+"""
+okulNumarasi = 271
+ad = "Emir"
+soyad = "Besi"
+sinavNotu = 99
+print("okul numarası", okulNumarasi," olan ", ad, soyad, sinavNotu)
+"""
+#endregion
+
+programlamaDili = "python"
+os = "10"
+ver = "3.9.2"
+
+# windows versiyonu 10 python versiyonu 3.9.2
+print("windows versiyonu " + os + " " + programlamaDili, " versiyonu " + ver)
+
+
+# istanbul ile danimarka arası 2800 km.
---
+++ 01_python_giris/0120_swap.py
@@ -0,0 +1,30 @@
+# değişken yer değiştirme
+
+a = 15
+b = 25
+"""
+temp = a
+a = b
+b = temp
+"""
+a, b = b, a
+print("a değeri", a, " b değeri", b)
+# a değeri 25, b değeri 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
